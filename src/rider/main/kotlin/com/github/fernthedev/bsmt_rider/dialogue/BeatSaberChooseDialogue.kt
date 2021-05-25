e57811e7b2bf371e2a82744a24be65d53fc15da2
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-05-25 09:26:51 -0400

    Use modern browsable folder combo box

--- src/rider/main/kotlin/com/github/fernthedev/bsmt_rider/dialogue/BeatSaberChooseDialogue.kt
+++ src/rider/main/kotlin/com/github/fernthedev/bsmt_rider/dialogue/BeatSaberChooseDialogue.kt
@@ -1,15 +1,20 @@
 package com.github.fernthedev.bsmt_rider.dialogue
 
 import com.github.fernthedev.bsmt_rider.settings.AppSettingsState
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.ComboboxWithBrowseButton
+import com.intellij.ui.components.fields.ExtendableTextComponent
+import com.intellij.ui.components.fields.ExtendableTextField
 import com.intellij.util.ui.FormBuilder
 import java.awt.BorderLayout
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.JTextField
+import javax.swing.plaf.basic.BasicComboBoxEditor
 
 
 class BeatSaberChooseDialogue(val project: Project?) : DialogWrapper(project) {
@@ -23,14 +28,30 @@ class BeatSaberChooseDialogue(val project: Project?) : DialogWrapper(project) {
     override fun createCenterPanel(): JComponent {
         beatSaberInput = ComboBox(AppSettingsState.instance.beatSaberFolders)
 
-        val comboBoxParent = ComboboxWithBrowseButton(beatSaberInput)
-        comboBoxParent.componentPopupMenu?.isVisible = AppSettingsState.instance.beatSaberFolders.isNotEmpty()
-        comboBoxParent.addBrowseFolderListener(project, FileChooserDescriptorFactory.createSingleFolderDescriptor())
+        val browseExtension = ExtendableTextComponent.Extension.create(
+            AllIcons.General.OpenDisk, AllIcons.General.OpenDiskHover,
+            "Open folder"
+        ) {
+            FileChooser.chooseFile(FileChooserDescriptorFactory.createSingleFolderDescriptor(), project, null) {
+                beatSaberInput.selectedItem = it.canonicalPath
+            }
+        }
 
+        beatSaberInput.editor = object : BasicComboBoxEditor() {
+            override fun createEditorComponent(): JTextField {
+                val ecbEditor = ExtendableTextField()
+                ecbEditor.addExtension(browseExtension)
+                ecbEditor.border = null
+                return ecbEditor
+            }
+        }
+
+
+        beatSaberInput.componentPopupMenu?.isVisible = AppSettingsState.instance.beatSaberFolders.isNotEmpty()
         beatSaberInput.isEditable = true
 
         return FormBuilder.createFormBuilder()
-            .addLabeledComponent("Beat Saber directory", comboBoxParent, 1, true)
+            .addLabeledComponent("Beat Saber directory", beatSaberInput, 1, true)
             .addComponentFillVertically(JPanel(BorderLayout()), 0)
             .panel
     }
