9c3e729017e2846bba6d2c96decd60ae6bd6a9aa
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-11-12 10:51:13 -0400

    Bump for 2021.3 EAP7

--- src/dotnet/ReSharperPlugin.BSMT/ModVersionTemplateParameter.cs
+++ src/dotnet/ReSharperPlugin.BSMT/ModVersionTemplateParameter.cs
@@ -1,10 +1,7 @@
 using System.Collections.Generic;
-using System.Linq;
-using JetBrains.Application;
-using JetBrains.ReSharper.Host.Features.ProjectModel.ProjectTemplates.DotNetExtensions;
-using JetBrains.ReSharper.Host.Features.ProjectModel.ProjectTemplates.DotNetTemplates;
+using JetBrains.Rider.Backend.Features.ProjectModel.ProjectTemplates.DotNetExtensions;
+using JetBrains.Rider.Backend.Features.ProjectModel.ProjectTemplates.DotNetTemplates;
 using JetBrains.Rider.Model;
-using JetBrains.Util;
 
 
 namespace ReSharperPlugin.BSMT_Rider
--- src/rider/main/kotlin/com/github/fernthedev/bsmt_rider/settings/AppSettingsConfigurable.kt
+++ src/rider/main/kotlin/com/github/fernthedev/bsmt_rider/settings/AppSettingsConfigurable.kt
@@ -17,7 +17,8 @@ class AppSettingsConfigurable : Configurable {
 
     // A default constructor with no arguments is required because this implementation
     // is registered as an applicationConfigurable EP
-    override fun getDisplayName(): @Nls(capitalization = Nls.Capitalization.Title) String {
+    @Nls(capitalization = Nls.Capitalization.Title)
+    override fun getDisplayName(): String {
         return "BSMT Rider: Settings"
     }
 
